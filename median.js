5250ef369e18ffe9b92d5f77da7de952d44176c9
ally-myi <53482800+ally-myi@users.noreply.github.com>
2019-12-16 08:43:59 -0500

    Update median.js

--- median.js
+++ median.js
@@ -1,41 +1,9 @@
 <script>
-   import java.util.Scanner;
-public class PeopleWealth {
- 
- 
-	public static void main(String[] args){
-		Scanner scan = new Scanner(System.in);
- 
-		//Decide the number of friends
-		System.out.print("Enter how many people from their wealth: ");
-		int numOfWealth = Integer.parseInt(scan.nextLine());
- 
-		//Create a string array to store the names of your friends
-		String arrayOfWealth[] = new String[numOfWealth];
-		for (int i = 0; i < arrayOfWealth.length; i++) {
-			System.out.print("Enter the amount of wealth " + (i+1) + " : ");
-		        arrayOfWealth[i] = scan.nextLine();
-		}
-		    
-		//Now show your friend's name one by one
-		for (int i = 0; i < arrayOfWealth.length; i++) {
-			System.out.print("My Wealth " + (i+1) + " : ");
-		        System.out.print(arrayOfWealth[i] + "\n");
-		}
-		  
- 
-		    
- 
-	}
- 
-}
-   
-   
-   
-   
-   
-   
-   
-   
-   
+const median = arr => {
+  const mid = Math.floor(arr.length / 2),
+    nums = [...arr].sort((a, b) => a - b);
+  return arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
+};
+console.log(median([5, 6, 50, 1, -5]));
+console.log(median([1, 2, 3, 4, 5]));  
 </script>
