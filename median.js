ab25112d545dfa0035848b859b094da2fa6a2eb3
ally-myi <53482800+ally-myi@users.noreply.github.com>
2019-12-16 08:28:46 -0500

    Update median.js

--- median.js
+++ median.js
@@ -1,5 +1,34 @@
 <script>
-
+   import java.util.Scanner;
+public class PeopleWealth {
+ 
+ 
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+ 
+		//Decide the number of friends
+		System.out.print("Enter how many people from their wealth: ");
+		int numOfWealth = Integer.parseInt(scan.nextLine());
+ 
+		//Create a string array to store the names of your friends
+		String arrayOfWealth[] = new String[numOfWealth];
+		for (int i = 0; i < arrayOfWealth.length; i++) {
+			System.out.print("Enter the amount of wealth " + (i+1) + " : ");
+		        arrayOfWealth[i] = scan.nextLine();
+		}
+		    
+		//Now show your friend's name one by one
+		for (int i = 0; i < arrayOfWealth.length; i++) {
+			System.out.print("My Wealth " + (i+1) + " : ");
+		        System.out.print(arrayOfWealth[i] + "\n");
+		}
+		  
+ 
+		    
+ 
+	}
+ 
+}
    
    
    
