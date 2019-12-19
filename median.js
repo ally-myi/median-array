8811284233e4e9979117bd48506d17b3013c90d1
ally-myi <53482800+ally-myi@users.noreply.github.com>
2019-12-19 08:14:51 -0500

    Update median.js

--- median.js
+++ median.js
@@ -6,3 +6,24 @@ var nameInput  = document.getElementById("name");
 var amountInput   = document.getElementById("amount");
 
 var messageBox  = document.getElementById("display");
+function insert ( ) {
+ titles.push( titleInput.value );
+ names.push( nameInput.value );
+ tickets.push( ticketInput.value );
+  
+ clearAndShow();
+}
+
+function clearAndShow () {
+  // Clear our fields
+  titleInput.value = "";
+  nameInput.value = "";
+  ticketInput.value = "";
+  
+  // Show our output
+  messageBox.innerHTML = "";
+  
+  messageBox.innerHTML += "Titles: " + titles.join(", ") + "<br/>";
+  messageBox.innerHTML += "Names: " + names.join(", ") + "<br/>";
+  messageBox.innerHTML += "Tickets: " + tickets.join(", ");
+}
