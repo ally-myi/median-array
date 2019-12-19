1f244b73728586796fb0da4c9382b3789b671875
ally-myi <53482800+ally-myi@users.noreply.github.com>
2019-12-19 08:36:21 -0500

    Update median.js

--- median.js
+++ median.js
@@ -1,29 +1,24 @@
-<script>
-var names   = [];
-var amount = [];
 
-var nameInput  = document.getElementById("name");
-var amountInput   = document.getElementById("amount");
+var amounts = [];
+
+
+var amountInput = document.getElementById("amounts");
 
 var messageBox  = document.getElementById("display");
+
 function insert ( ) {
- titles.push( titleInput.value );
- names.push( nameInput.value );
- tickets.push( ticketInput.value );
+
+ amounts.push( amountInput.value );
   
  clearAndShow();
 }
 
 function clearAndShow () {
   // Clear our fields
-  titleInput.value = "";
-  nameInput.value = "";
-  ticketInput.value = "";
+  amountInput.value = "";
   
   // Show our output
   messageBox.innerHTML = "";
-  
-  messageBox.innerHTML += "Titles: " + titles.join(", ") + "<br/>";
-  messageBox.innerHTML += "Names: " + names.join(", ") + "<br/>";
-  messageBox.innerHTML += "Tickets: " + tickets.join(", ");
+ 
+  messageBox.innerHTML += "Amounts: " + amounts.join(", ");
 }
