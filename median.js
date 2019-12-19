de796d192ce8656371526d602176bc60958c0a34
ally-myi <53482800+ally-myi@users.noreply.github.com>
2019-12-19 08:12:47 -0500

    Update median.js

--- median.js
+++ median.js
@@ -1,9 +1,8 @@
 <script>
-const median = arr => {
-  const mid = Math.floor(arr.length / 2),
-    nums = [...arr].sort((a, b) => a - b);
-  return arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
-};
-console.log(median([5, 6, 50, 1, -5]));
-console.log(median([1, 2, 3, 4, 5]));  
-</script>
+var names   = [];
+var amount = [];
+
+var nameInput  = document.getElementById("name");
+var amountInput   = document.getElementById("amount");
+
+var messageBox  = document.getElementById("display");
